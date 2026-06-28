f1bde2c72f1c1013cf9a782ad7bafd9a74aeb2d1
aGamingGod1234 <agaminggod1234@gmail.com>
2026-06-28 21:54:33 +0800

    Force Node runtime for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import LandingPageClient from './LandingPageClient'
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
+export const runtime = 'nodejs'
 
 export default function LandingPage() {
   return <LandingPageClient />
